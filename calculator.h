0c66f666900bfc9ed413f6b9aac4ef8a4ce5141f
VarRoman <bezshchasnyiroman7a76@gmail.com>
2024-10-02 20:03:01 +0200

    Added extra lines after ending cherry-pick stuff

--- calculator.h
+++ calculator.h
@@ -5,7 +5,10 @@ class Calculator
 {
     public:
         int Add (int, int);
+  int Sub (double, double);
         int Mul (double, double);
+        int Div (double, double);
+        int Add_Plus_Sub(double, double, double);
 };
 
 #endif//CALCULATOR_H
